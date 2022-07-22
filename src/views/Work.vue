78472012b6d0952598623a3b98b299f9c7f47034
bewburats <bew25442@gmail.com>
2022-07-23 00:10:14 +0700

    add work and edit css

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -6,21 +6,21 @@
     <div class="bg-work w-screen h-screen bg-no-repeat bg-cover bg-center flex items-center">
         <div class="container mx-auto px-4 pt-16">
             <div class="flex flex-col gap-2 ">
-                <div class="relative mt-20 sm:mt-2">
-                <p
-                    class="text-center text-2xl sm:text-5xl md:text-5xl lg:text-7xl text-gray-900 font-black uppercase tracking-widest">
-                    WORK
-                </p>
+                <div class="relative  mt-72 sm:mt-2">
+                    <p
+                        class="text-center text-2xl sm:text-5xl md:text-5xl lg:text-7xl text-gray-900 font-black uppercase tracking-widest">
+                        WORK
+                    </p>
                 </div>
                 <p class="text-xl sm:text-3xl md:text-4xl lg:text-3xl text-gray-900 font-bold">
                     Ux / Ui Design</p>
                 <div class="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
 
                     <a href="https://www.figma.com/proto/BDhptEhGYyN8pSCC2RaBxQ/PROTOTYPE?node-id=1%3A2&starting-point-node-id=1%3A2"
-                        target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
+                        target="_blank" class="flex flex-col items-center bg-pink-200 sm:bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
                         <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
-                            src="../../public/images/jadeakpa.png" alt="">
+                            src="../../public/images/jadeakpa.png" alt="jadeakpa">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight ">JADEAKPA APP
                             </h5>
@@ -31,10 +31,10 @@
                     </a>
 
                     <a href="https://www.figma.com/proto/6H6uHXmppZAR7oigIunp1m/Temp2?scaling=scale-down&page-id=0%3A1&starting-point-node-id=0%3A3&node-id=0%3A3"
-                        target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
+                        target="_blank" class="flex flex-col items-center bg-purple-200 sm:bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
                         <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
-                            src="../../public/images/booking.png" alt="">
+                            src="../../public/images/booking.png" alt="booking">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">BOOKING</h5>
                             <p class="mb-3 font-normal">for booking and check
@@ -45,10 +45,10 @@
 
 
                     <a href="https://www.figma.com/proto/rJBFAxDA8NP12125kCed0x/Colors-V2?node-id=3%3A3&starting-point-node-id=3%3A3&scaling=scale-down"
-                        target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
+                        target="_blank" class="flex flex-col items-center bg-pink-200 sm:bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
                         <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
-                            src="../../public/images/colors.png" alt="">
+                            src="../../public/images/colors.png" alt="colors">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">COLORS</h5>
                             <p class="mb-3 font-normal">for explore trending and
@@ -58,10 +58,10 @@
                     </a>
 
                     <a href="https://www.figma.com/proto/xQEdwhX2dPS4fmQK6NZPVB/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1"
-                        target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
+                        target="_blank" class="flex flex-col items-center  bg-purple-200 sm:bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
                         <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
-                            src="../../public/images/bewyour.png" alt="">
+                            src="../../public/images/bewyour.png" alt="bewyour">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">MY PORTFOLIO
                             </h5>
@@ -74,66 +74,96 @@
                 </div>
                 <p class="text-xl sm:text-3xl md:text-4xl mt-3 lg:text-3xl text-gray-900 font-bold">
                     Designs
-                 
-                    </p>
+
+                </p>
                 <div class="grid sm:grid-cols-1 md:grid-cols-7 gap-4">
-                    <a href="https://drive.google.com/drive/folders/1sny0OZOknhPwk5yaY-CAzbnO2VwfadTJ?usp=sharing" target="_blank" 
-                    class="flex flex-col items-center  rounded-lg shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg shadow-md bg-blue-600 sm:bg-white
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <p class="md:hidden text-xl p-4 leading-normal font-bold" >Google Drive Link →</p>
-                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg" src="../../public/images/colors-logo.png" alt="">
-                        <!-- <img class="md:hidden w-2/12 rounded-lg" src="../../public/images/cpe-freshy.jpg" alt=""> -->
+                        <img src="../../public/images/behance.svg"
+                            class="md:hidden w-10/12 text-xl p-4 leading-normal font-bold" alt="behance">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/jelly-mascot.png" alt="mascot">
                     </a>
 
-                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/cpe-freshy.png" alt="">
+                            src="../../public/images/cpe-freshy.png" alt="cpe-freshy">
                     </a>
 
-                    
 
-                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/lawyer-logo.png" alt="">
+                            src="../../public/images/lawyer-logo.png" alt="lawyer-logo">
                     </a>
 
-                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/csa-logo.png" alt="">
+                            src="../../public/images/csa-logo.png" alt="csa-logo">
                     </a>
 
-                    <a href="#" target="_blank"   class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/thungting-kmutt.png" alt="">
+                            src="../../public/images/thungting-kmutt.png" alt="thungting-kmutt">
                     </a>
 
-                    <a href="#" target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/rabbit-card.png" alt="">
+                            src="../../public/images/rabbit-card.png" alt="rabbit-card">
                     </a>
 
-                    <a href="#"  target="_blank" class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
+                    <a href="https://www.behance.net/buratsboonma" target="_blank" class="zoom flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
-                            src="../../public/images/ice-bill.png" alt="">
+                            src="../../public/images/ice-bill.png" alt="ice-bill">
                     </a>
                 </div>
-                </div>
-                 <a href="https://www.behance.net/buratsboonma" target="_blank">
-  <p class="hidden text-base font-normal md:block link link-hover">
-    <span class=" text-purple-400">more in </span>
-    <span class=" text-pink-400">Behance ! →</span>
-     </p></a>
             </div>
+            <a href="https://www.behance.net/buratsboonma" target="_blank">
+                <p class="hidden mt-5 text-base font-semibold md:block link link-hover">
+                    <span class=" text-purple-400">more in </span>
+                    <span class=" text-pink-400">Behance! →</span>
+                </p>
+            </a>
         </div>
+    </div>
 
     <!-- </div> -->
 </template>
  
 <style>
+.zoom {
+
+    transition: transform .2s;
+
+}
+
+.zoom:hover {
+    transform: scale(1.5);
+}
+
+@keyframes flipInX {
+    0% {
+        transform: perspective(400px) rotateX(90deg);
+        opacity: 0;
+    }
+
+    40% {
+        transform: perspective(400px) rotateX(-10deg);
+    }
+
+    70% {
+        transform: perspective(400px) rotateX(10deg);
+    }
+
+    100% {
+        transform: perspective(400px) rotateX(0deg);
+        opacity: 1;
+    }
+}
 </style>
