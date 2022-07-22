d675d823c3a5f910c6d192b16a10562c7d2912f9
bewburats <bew25442@gmail.com>
2022-07-22 17:49:44 +0700

    edit css background color

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,18 +4,21 @@
  
 <template>
 
-<nav class="bg-transparent lg:text-black sm:px-4 absolute z-50 w-full rounded-b-2xl p-1 mt-5">
+<nav class=" bg-pink-50 xs:bg-white sm:bg-white md:bg-transparent lg:bg-transparent lg:text-white sm:px-4 absolute z-50 w-full  p-1">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
     <router-link :to="{name : 'home'}" class="flex items-center">
         
-        <span class="self-center text-xl font-bold whitespace-nowrap dark:text-white">Burats</span>
+        <span class="mt-2 xs:mt-5 sm:mt-5 lg:mt-5 md:mt-5 self-center text-xl  text-black font-bold whitespace-nowrap dark:text-white">Burats</span>
     </router-link>
     
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm hover:bg-transparent text-purple-400 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span class="sr-only">Open main menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+
+
+
+    <div class="mt-5 hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
         <li>
            <router-link :to="{name : 'home'}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</router-link>
