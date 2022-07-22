03ade346756763feba97661979f5b35abc9b2357
bewburats <bew25442@gmail.com>
2022-07-22 17:50:05 +0700

    edit css responsive

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -5,19 +5,22 @@
 <template>
     <div class="bg-work w-screen h-screen bg-no-repeat bg-cover bg-center flex items-center">
         <div class="container mx-auto px-4 pt-16">
-            <div class="flex flex-col gap-6 ">
+            <div class="flex flex-col gap-2 ">
+                <div class="relative mt-20 sm:mt-2">
                 <p
-                    class="text-center text-3xl xs:text-4xl sm:text-5xl md:text-5xl lg:text-7xl text-gray-900 font-black uppercase tracking-widest">
+                    class="text-center text-2xl sm:text-5xl md:text-5xl lg:text-7xl text-gray-900 font-black uppercase tracking-widest">
                     WORK
                 </p>
-                <p class="text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-3xl text-gray-900 font-bold">
+                </div>
+                <p class="text-xl sm:text-3xl md:text-4xl lg:text-3xl text-gray-900 font-bold">
                     Ux / Ui Design</p>
                 <div class="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
 
                     <a href="https://www.figma.com/proto/BDhptEhGYyN8pSCC2RaBxQ/PROTOTYPE?node-id=1%3A2&starting-point-node-id=1%3A2"
                         target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
-                        <img class="m-4 object-cover w-2/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
+                            src="../../public/images/jadeakpa.png" alt="">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight ">JADEAKPA APP
                             </h5>
@@ -30,23 +33,26 @@
                     <a href="https://www.figma.com/proto/6H6uHXmppZAR7oigIunp1m/Temp2?scaling=scale-down&page-id=0%3A1&starting-point-node-id=0%3A3&node-id=0%3A3"
                         target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
-                        <img class=" m-4 object-cover w-2/12 rounded-lg" src="../../public/images/booking.png" alt="">
+                        <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
+                            src="../../public/images/booking.png" alt="">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">BOOKING</h5>
-                            <p class="mb-3 font-normal">for booking clinic and check
+                            <p class="mb-3 font-normal">for booking and check
                                 available schedule.</p>
                             <p class="font-normal underline">figma link →</p>
                         </div>
                     </a>
 
+
                     <a href="https://www.figma.com/proto/rJBFAxDA8NP12125kCed0x/Colors-V2?node-id=3%3A3&starting-point-node-id=3%3A3&scaling=scale-down"
                         target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
-                        <img class="m-4 object-cover w-2/12 rounded-lg" src="../../public/images/colors.png" alt="">
+                        <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
+                            src="../../public/images/colors.png" alt="">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">COLORS</h5>
-                            <p class="mb-3 font-normal">for explore trending color and
-                                generate your colors.</p>
+                            <p class="mb-3 font-normal">for explore trending and
+                                generate colors.</p>
                             <p class="font-normal underline">figma link →</p>
                         </div>
                     </a>
@@ -54,7 +60,8 @@
                     <a href="https://www.figma.com/proto/xQEdwhX2dPS4fmQK6NZPVB/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1"
                         target="_blank" class="flex flex-col items-center bg-white hover:bg-purple-500 rounded-lg border shadow-md 
                         md:flex-row text-gray-800 hover:text-white dark:border-gray-700">
-                        <img class="m-4 object-cover w-2/12 rounded-lg" src="../../public/images/bewyour.png" alt="">
+                        <img class="m-4 hidden object-cover md:w-3/12 lg:w-3/12 xl:w-2/12 md:block rounded-lg"
+                            src="../../public/images/bewyour.png" alt="">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight">MY PORTFOLIO
                             </h5>
@@ -65,49 +72,67 @@
                     </a>
 
                 </div>
-                <p class="text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-3xl text-gray-900 font-bold">
-                    Designs</p>
-                <div class="grid sm:grid-cols-2 md:grid-cols-7 gap-4">
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                <p class="text-xl sm:text-3xl md:text-4xl mt-3 lg:text-3xl text-gray-900 font-bold">
+                    Designs
+                 
+                    </p>
+                <div class="grid sm:grid-cols-1 md:grid-cols-7 gap-4">
+                    <a href="https://drive.google.com/drive/folders/1sny0OZOknhPwk5yaY-CAzbnO2VwfadTJ?usp=sharing" target="_blank" 
+                    class="flex flex-col items-center  rounded-lg shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/cpe-freshy.jpg" alt="">
+                        <p class="md:hidden text-xl p-4 leading-normal font-bold" >Google Drive Link →</p>
+                         <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg" src="../../public/images/colors-logo.png" alt="">
+                        <!-- <img class="md:hidden w-2/12 rounded-lg" src="../../public/images/cpe-freshy.jpg" alt=""> -->
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/lawyer-logo.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/cpe-freshy.png" alt="">
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    
+
+                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/lawyer-logo.png" alt="">
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    <a href="#"  target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/csa-logo.png" alt="">
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    <a href="#" target="_blank"   class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/thungting-kmutt.png" alt="">
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    <a href="#" target="_blank"  class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/rabbit-card.png" alt="">
                     </a>
 
-                    <a href="#" class="flex flex-col items-center  rounded-lg border shadow-md 
+                    <a href="#"  target="_blank" class="flex flex-col items-center  rounded-lg hidden:border  shadow-md 
                           hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="w-12/12 rounded-lg" src="../../public/images/jadeakpa.png" alt="">
+                        <img class="hidden object-cover md:w-10/12 lg:w-8/12 xl:w-auto md:block rounded-lg"
+                            src="../../public/images/ice-bill.png" alt="">
                     </a>
-
                 </div>
+                </div>
+                 <a href="https://www.behance.net/buratsboonma" target="_blank">
+  <p class="hidden text-base font-normal md:block link link-hover">
+    <span class=" text-purple-400">more in </span>
+    <span class=" text-pink-400">Behance ! →</span>
+     </p></a>
             </div>
         </div>
 
-    </div>
+    <!-- </div> -->
 </template>
  
 <style>
